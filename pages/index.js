8f2b2dcc341696a79f7697f67fc2a277b2922047
Ahmad <92533229+ahmadps@users.noreply.github.com>
2025-04-28 16:22:28 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -151,20 +151,5 @@ export default function Home() {
        </section>
       </main>
     </div>
-<script src="three.r134.min.js"></script>
-<script src="vanta.net.min.js"></script>
-<script>
-VANTA.NET({
-  el: "#your-element-selector",
-  mouseControls: true,
-  touchControls: true,
-  gyroControls: false,
-  minHeight: 200.00,
-  minWidth: 200.00,
-  scale: 1.00,
-  scaleMobile: 1.00,
-  color: 0x5f74ff
-})
-</script>
   );
 }
